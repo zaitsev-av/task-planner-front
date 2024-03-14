e2d8a05fd78b510d9a77c3260bc1d2ef78982424
zaitsev-av <zaitsev.av.12@gmail.com>
2024-03-14 20:29:15 +0300

    feat: password icon

--- src/assets/icons/password.tsx
+++ src/assets/icons/password.tsx
@@ -1,23 +1,42 @@
 import * as React from 'react'
 import { SVGProps, Ref, forwardRef, memo } from 'react'
 
-const SvgComponent = (
-	props: SVGProps<SVGSVGElement>,
-	ref: Ref<SVGSVGElement>
-) => (
-	<svg
-		xmlns='http://www.w3.org/2000/svg'
-		width='1em'
-		height='1em'
-		viewBox='0 0 24 24'
-		ref={ref}
-		{...props}
-	>
-		<path
-			fill='currentColor'
-			d='M12 17a2 2 0 0 0 2-2 2 2 0 0 0-2-2 2 2 0 0 0-2 2 2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5 5 5 0 0 1 5 5v2zm-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3'
-		/>
-	</svg>
-)
+type Props = SVGProps<SVGSVGElement> & { isVisible: boolean }
+const SvgComponent = (props: Props, ref: Ref<SVGSVGElement>) => {
+	const { isVisible, ...rest } = props
+	return (
+		<>
+			{isVisible ? (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					{...rest}
+					ref={ref}
+				>
+					<path
+						fill='currentColor'
+						d='M12 6C5.188 6 1 12 1 12s4.188 6 11 6s11-6 11-6s-4.188-6-11-6m0 10c-3.943 0-6.926-2.484-8.379-4c1.04-1.085 2.862-2.657 5.254-3.469A3.96 3.96 0 0 0 8 11a4 4 0 0 0 8 0a3.96 3.96 0 0 0-.875-2.469c2.393.812 4.216 2.385 5.254 3.469c-1.455 1.518-4.437 4-8.379 4'
+					/>
+				</svg>
+			) : (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					{...rest}
+					ref={ref}
+				>
+					<path
+						fill='currentColor'
+						d='M1 12s4.188-6 11-6c.947 0 1.839.121 2.678.322L8.36 12.64A3.96 3.96 0 0 1 8 11c0-.937.335-1.787.875-2.469c-2.392.812-4.214 2.385-5.254 3.469a14.868 14.868 0 0 0 2.98 2.398l-1.453 1.453C2.497 14.13 1 12 1 12m22 0s-4.188 6-11 6c-.946 0-1.836-.124-2.676-.323L5 22l-1.5-1.5l17-17L22 5l-3.147 3.147C21.501 9.869 23 12 23 12m-2.615.006a14.83 14.83 0 0 0-2.987-2.403L16 11a4 4 0 0 1-4 4l-.947.947c.31.031.624.053.947.053c3.978 0 6.943-2.478 8.385-3.994'
+					/>
+				</svg>
+			)}
+		</>
+	)
+}
 const ForwardRef = forwardRef(SvgComponent)
 export default memo(ForwardRef)
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,5 +1,5 @@
 import { clsx } from 'clsx'
-import { forwardRef, InputHTMLAttributes, Ref } from 'react'
+import { forwardRef, InputHTMLAttributes, Ref, useState } from 'react'
 
 import { PasswordIcon } from '../../assets/icons'
 
@@ -37,10 +37,12 @@ export const Input = forwardRef(
 		}: InputPropsType,
 		forwardRef: Ref<HTMLInputElement>
 	) => {
+		const [isVisible, setIsVisible] = useState<boolean>(false)
 		const classNames = {
 			box: clsx(style.box),
 			field: clsx(style.field),
-			label: clsx(style.label)
+			label: clsx(style.label),
+			icon: clsx(style.icon)
 		}
 		//todo если есть данные в инпуте, и используется label то не опускать его на текст внутри инпута, оставить его в верхнем положении
 		//todo добавить иконки
@@ -49,7 +51,7 @@ export const Input = forwardRef(
 			<div className={classNames.box}>
 				<input
 					className={classNames.field}
-					type={type}
+					type={isVisible ? 'text' : type}
 					ref={forwardRef}
 					placeholder={placeholder}
 					{...rest}
@@ -57,7 +59,14 @@ export const Input = forwardRef(
 				{withLabel && !placeholder && (
 					<label className={classNames.label}>{labelText}</label>
 				)}
-				<PasswordIcon />
+				{/*{isVisible && (*/}
+				<PasswordIcon
+					onClick={() => setIsVisible(prevState => !prevState)}
+					isVisible={isVisible}
+					className={classNames.icon}
+					data-icon={type === 'password' ? 'password' : 'none'}
+				/>
+				{/*)}*/}
 			</div>
 		)
 	}
