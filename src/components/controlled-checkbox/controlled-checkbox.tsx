4d6a1490f82493e53a05967954d83b99c8ad1f25
zaitsev-av <zaitsev.av.12@gmail.com>
2024-03-24 17:12:14 +0300

    feat: add controller for checkbox

---
+++ src/components/controlled-checkbox/controlled-checkbox.tsx
@@ -0,0 +1,34 @@
+import {
+	FieldValues,
+	useController,
+	UseControllerProps
+} from 'react-hook-form';
+
+import { Checkbox, ICheckboxProps } from '@/components';
+
+type ControlledCheckboxType<T extends FieldValues> = UseControllerProps<T> &
+	ICheckboxProps;
+export const ControlledCheckbox = <T extends FieldValues>({
+	control,
+	defaultValue,
+	shouldUnregister,
+	disabled,
+	name,
+	rules,
+	...rest
+}: ControlledCheckboxType<T>) => {
+	const { field } = useController({
+		control,
+		defaultValue,
+		shouldUnregister,
+		disabled,
+		name,
+		rules
+	});
+	return (
+		<Checkbox
+			{...field}
+			{...rest}
+		/>
+	);
+};
